1077093b6f2ca811cdce209ce75040fa0ae10fae
Aiska2005 <aiskajumantaeva5@gmail.com>
2025-07-24 11:24:06 -0500

    added new date

--- Header/app.js
+++ Header/app.js
@@ -1,7 +1,7 @@
-function handleRefresh() {
-  console.log("Refresh clicked!");
-  alert("Data refreshed (mock)");
-}
+// function handleRefresh() {
+//   console.log("Refresh clicked!");
+//   alert("Data refreshed (mock)");
+// }
 
 // Optional: auto-update date
 document.getElementById("today-date").textContent =
